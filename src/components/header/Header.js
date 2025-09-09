5fec4b78d9eef64992e578b6ab6a63f2352481ae
Shonazarov-Sanjarbek <shonazarovsanjarbek71@gmail.com>
2025-09-09 14:12:49 +0500

    allbirds

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,22 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../assets/logo.png";
 
 function Header({ items, text }) {
-
+  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
     <div className="container">
       <div className="navbar">
-        <ul className='navbar__list'>
+        {/* Logo */}
+        <div className="navbar__logo">
+          <img src={logo} alt="logo" />
+        </div>
+
+        {/* Menu list */}
+        <ul className={`navbar__list ${menuOpen ? "show" : ""}`}>
           {items.map((item, index) => (
             <li className="navbar__item" key={index}>
               {item}
             </li>
           ))}
         </ul>
-        <div className="navbar__logo">
-          <img src={logo} alt="logo" />
-        </div>
+
+        {/* Icons */}
         <div className="navbar__icons">
           <div className="navbar__icons__text">
             {text.map((item, index) => (
@@ -24,13 +29,16 @@ function Header({ items, text }) {
             ))}
           </div>
           <div className="navbar__icons__icon">
-            <i class="fa-solid fa-user"></i>
-            <i class="fa-solid fa-circle-question"></i>
-            <i class="fa-solid fa-cart-shopping"></i>
+            <i className="fa-solid fa-user"></i>
+            <i className="fa-solid fa-circle-question"></i>
+            <i className="fa-solid fa-cart-shopping"></i>
           </div>
         </div>
+
+        {/* Burger */}
         <div
-          className='navbar__burger'
+          className={`navbar__burger ${menuOpen ? "active" : ""}`}
+          onClick={() => setMenuOpen(!menuOpen)}
         >
           <div></div>
           <div></div>
